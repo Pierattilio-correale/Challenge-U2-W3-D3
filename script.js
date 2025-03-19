944f912fcfca1c27830750513c5520c0722065da
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-03-19 16:55:47 +0100

    adds try on extra

--- script.js
+++ script.js
@@ -35,8 +35,32 @@ const getBoocks = function () {
           });
         });
       });
-      const myRow2 = document.getElementById("mybigrow2");
-      const myButtonB = document.querySelectorAll(".buycard");
+      //   costruzione carrello
+      //   const myRow2 = document.getElementById("mybigrow2");
+
+      //   myRow2.innerHTML = "";
+      //   data.forEach((book) => {
+      //     myRow2.innerHTML += `<div class="col  col-12 text-center my-4 d-flex align-items-stretch d-none coldelete" >
+      //         <div class="card  w-100 " >
+      //           <img src="${book.img}" class="card-img-top " alt="Copertina di ${book.title}">
+      //           <div class="card-body d-flex flex-column">
+      //             <h5 class="card-title flex-grow-1 mt-4">${book.title}</h5>
+      //             <p class="card-text">${book.price}€</p>
+      //             <div class="d-flex  ">
+
+      //             <a href="#" class="btn btn-primary mt-auto ms-1 ms-xl-2 mx-xxl-3 buycard " >rip card</a>
+      //               </div>
+      //           </div>
+      //         </div>
+      //       </div> `;
+      //     const myButtonB = document.querySelectorAll(".buycard");
+      //     myButtonB.forEach((btn, i) => {
+      //       btn.addEventListener("click", function () {
+      //         const carrello = document.querySelectorAll(".coldelete");
+      //         carrello[i].classList.remove("d-none");
+      //       });
+      //     });
+      //   });
     })
     .catch((err) => {
       console.log("errore", err);
